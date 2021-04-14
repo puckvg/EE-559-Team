20f9f2a5872d401ed7f7f51afc822772c8bd1635
felixelex <60747489+felixelex@users.noreply.github.com>
2021-04-15 00:53:28 +0200

    add verbose argument to trainer

--- Project1/src/trainer.py
+++ Project1/src/trainer.py
@@ -1,11 +1,12 @@
 import torch
 
 class Trainer:
-    def __init__(self, nb_epochs):
+    def __init__(self, nb_epochs, verbose=True):
         """ Create a trainer by specifying the number of epochs to train """
         self.nb_epochs = nb_epochs
+        self.verbose = verbose
     
-    def fit(self, model, dl_train, dl_val):
+    def fit(self, model, dl_train, dl_val, verbose=True):
         """ Train the model on the specified data and print the training and validation loss and accuracy.
         Args:
             model: Module. Model to train
@@ -13,6 +14,8 @@ class Trainer:
             dl_val: DataLoader. DataLoader containting the validation data
 
         """
+        self.verbose = verbose
+        
         optimizer = model.configure_optimizers()
         for e in range(self.nb_epochs):
             loss_train = []
@@ -26,16 +29,17 @@ class Trainer:
 
             loss_val = []
             acc_val = []
-            for batch_idx, batch in enumerate(dl_val):
-                model.eval()
-                with torch.no_grad():
-                    loss, acc = model.validation_step(batch, batch_idx)
-                    loss_val.append(loss.item())
-                    acc_val.append(acc)
-            avg_loss_train = round(sum(loss_train)/len(loss_train), 2)
-            avg_loss_val = round(sum(loss_val)/len(loss_val), 2)
-            avg_acc_val = round(sum(acc_val)/len(acc_val), 2)
-            print(f'# Epoch {e+1}/{self.nb_epochs}:\t loss={avg_loss_train}\t loss_val={avg_loss_val}\t acc_val={avg_acc_val}')
+            if self.verbose:
+                for batch_idx, batch in enumerate(dl_val):
+                    model.eval()
+                    with torch.no_grad():
+                        loss, acc = model.validation_step(batch, batch_idx)
+                        loss_val.append(loss.item())
+                        acc_val.append(acc)
+                avg_loss_train = round(sum(loss_train)/len(loss_train), 2)
+                avg_loss_val = round(sum(loss_val)/len(loss_val), 2)
+                avg_acc_val = round(sum(acc_val)/len(acc_val), 2)
+                print(f'# Epoch {e+1}/{self.nb_epochs}:\t loss={avg_loss_train}\t loss_val={avg_loss_val}\t acc_val={avg_acc_val}')
 
     def test(self, model, dl_test):
         """ Test the model on the specified data 
