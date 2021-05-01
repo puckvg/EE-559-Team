2743b4451c00a69bf8ebd2474a306e28a6c8abba
felixelex <60747489+felixelex@users.noreply.github.com>
2021-05-01 11:06:15 +0200

    Implemented functionals

--- Project2/nn/functional.py
+++ Project2/nn/functional.py
@@ -1,10 +1,77 @@
 import torch
 
 """ The functional.py should contain the concrete implementations of the specific loss functions. """
+# IT ALSO SHOULD CONTAIN OTHER USEFUL FUNCTIONS RIGHT? 
 
 def mse(x, y, reduction='mean'):
-    raise NotImplementedError
+    """ Compute the mean squared error.
+        Args:
+            x: torch.tensor. Input tensor.
+            y: torch.tensor. Target tensor.
+
+        Returns:
+            ms_error: float. Mean squared error.
+    """
+    ms_error = (x- y).norm(p=2).pow(2)
+    return ms_error
 
 
 def d_mse(x, y, reduction='mean'):
-    raise NotImplementedError
+    """ Compute the gradient of the mean squared error.
+        Args:
+            x: torch.tensor. Input tensor.
+            y: torch.tensor. Target tensor.
+
+        Returns:
+            d_mse: float. Gradient of mean squared error.
+    """
+    d_mse = 2*(x - y)
+    return d_mse
+
+
+def tanh(x):
+    """ Compute tanh(x).
+    Args:
+        x: torch.tensor. Input tensor
+    
+    Returns:
+        out: torch.tensor. Output tensor
+    """
+    out = x.tanh()
+    return out
+
+
+def d_tanh(x):
+    """ Compute gradient of tanh(x)
+    Args:
+        x: torch.tensor. Input tensor
+        
+    Returns:
+        out: torch.tensor. Output tensor
+    """ 
+    out = 1 - x.tanh().pow(2)
+    return out
+
+
+def relu(x):
+    """ Compute ReLU(x)
+    Args:
+        x: torch.tensor. Input tensor
+        
+    Returns:
+        out: torch.tensor. Output tensor
+    """
+    out = x.relu()
+    return out
+
+
+def d_relu(x):
+    """ Compute gradient of ReLU(x)
+    Args:
+        x: torch.tensor. Input tensor
+        
+    Returns:
+        out: torch.tensor. Output tensor
+    """
+    out = (x > 0) * 1
+    return out
