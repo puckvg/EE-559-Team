474ff7444df16ac5d5517b4d586253d4d84e6e5d
felixelex <60747489+felixelex@users.noreply.github.com>
2021-05-01 11:29:48 +0200

    Started with activations (not working yet)

--- Project2/nn/activation.py
+++ Project2/nn/activation.py
@@ -1,13 +1,12 @@
 import torch
 from nn.module import Module
-from nn.functional import *
+import nn.functional as f
 
 class Activation(Module):
-    def forward(self, x, y):
+    def forward(self, x):
         """ Compute the activation.
         Args:
             x: torch.tensor. Input tensor.
-            y: torch.tensor. Target tensor.
         """
         pass
 
@@ -21,3 +20,33 @@ class Activation(Module):
             y: torch.tensor. Target tensor.
         """
         pass
+    
+    
+class ReLU(Activation):
+    
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, x):
+        """ Compute the activation.
+        Args:
+            x: torch.tensor. Input tensor.
+        
+        Returns:
+            out: torch.tensor. Output tensor
+        """
+        out = f.relu(x)
+        return out
+    
+    def backward(self):
+        """ Compute gradients of input.
+        Args:
+            x: torch.tensor. Input tensor.
+            
+        Returns:
+            grad: torch.tensor. Gradient
+        """
+        grad = f.d_relu(x)
+        return grad
+        
+    
