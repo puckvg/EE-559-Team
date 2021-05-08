9dbe9a5977137a11427bf4b08fdc32e9af8e77a5
felixelex <60747489+felixelex@users.noreply.github.com>
2021-05-08 09:47:55 +0200

    cleaned test_linear

--- Project2/test_linear.py
+++ Project2/test_linear.py
@@ -13,9 +13,9 @@ max_batch_size = 100
 thresh = 1e-3
 
 class TestLinear(TestModule):
-    def _forward(self, in_dim, out_dim):
+    def _forward(self, batch_size, in_dim, out_dim):
         # Generate random test data
-        x, y = self._gen_data(in_dim, out_dim)
+        x, y = self._gen_batch_data(batch_size, in_dim, out_dim)
 
         # Initialize modules
         mod_ours, mod_theirs = self._init_modules(in_dim, out_dim)
@@ -29,17 +29,18 @@ class TestLinear(TestModule):
 
     def test_random_forward(self):
         for _ in range(n_tests):
+            batch_size = random.randint(1, max_batch_size)
             in_dim = random.randint(1, max_dim)
             out_dim = random.randint(1, max_dim)
-            self._forward(in_dim, out_dim)
-            
-    def _backward(self, in_dim, out_dim):
-        mod_ours, mod_theirs = self._forward(in_dim, out_dim)
+            self._forward(batch_size, in_dim, out_dim)
+    
+    def _backward(self, batch_size, in_dim, out_dim):
+        mod_ours, mod_theirs = self._forward(batch_size, in_dim, out_dim)
         
         loss_fn_ours = MSELoss()
         loss_fn_theirs = torch.nn.MSELoss()
 
-        x, y = self._gen_data(in_dim, out_dim)
+        x, y = self._gen_batch_data(batch_size, in_dim, out_dim)
 
         out_ours = mod_ours(x)
         out_theirs = mod_theirs(x)
@@ -61,73 +62,12 @@ class TestLinear(TestModule):
         for _ in range(n_tests):
             in_dim = 3
             out_dim = 2
-            self._backward(in_dim, out_dim)
+            batch_size = 2
+            self._backward(batch_size, in_dim, out_dim)
     
     def test_random_backward(self):
         for _ in range(n_tests):
             in_dim = random.randint(1, max_dim)
             out_dim = random.randint(1, max_dim)
-            self._backward(in_dim, out_dim)
-    
-    def _forward_batch(self, batch_size, in_dim, out_dim):
-        # Generate random test data
-        x, y = self._gen_batch_data(batch_size, in_dim, out_dim)
-
-        # Initialize modules
-        mod_ours, mod_theirs = self._init_modules(in_dim, out_dim)
-
-        # Compute forward pass
-        out_ours = mod_ours(x)
-        out_theirs = mod_theirs(x)
-
-        assert out_ours.isclose(out_theirs, rtol=thresh).all(), 'Outputs of linear must be the same'
-        return mod_ours, mod_theirs
-
-    def test_random_forward_batch(self):
-        for _ in range(n_tests):
             batch_size = random.randint(1, max_batch_size)
-            in_dim = random.randint(1, max_dim)
-            out_dim = random.randint(1, max_dim)
-            self._forward_batch(batch_size, in_dim, out_dim)
-    
-    def _backward_batch(self, batch_size, in_dim, out_dim):
-        mod_ours, mod_theirs = self._forward(in_dim, out_dim)
-        
-        loss_fn_ours = MSELoss()
-        loss_fn_theirs = torch.nn.MSELoss()
-
-        x, y = self._gen_batch_data(batch_size, in_dim, out_dim)
-
-        out_ours = mod_ours(x)
-        out_theirs = mod_theirs(x)
-
-        loss_ours = loss_fn_ours(out_ours, y)
-        loss_theirs = loss_fn_theirs(out_theirs, y)
-
-        dy = loss_fn_ours.backward()
-        mod_ours.backward(dy)
-
-        loss_fn_theirs = torch.nn.MSELoss()
-        loss_theirs = loss_fn_theirs(out_theirs, y)
-        loss_theirs.backward()
-
-        assert mod_ours.cache['dw_glob'].isclose(mod_theirs.weight.grad, rtol=thresh).all(), 'Gradient of weights must be equal'
-        assert mod_ours.cache['db_glob'].isclose(mod_theirs.bias.grad, rtol=thresh).all(), 'Gradient of bias must be equal'
-    
-    def test_small_backward_batch(self):
-        for _ in range(n_tests):
-            in_dim = 3
-            out_dim = 2
-            batch_size = 2
-            self._backward_batch(batch_size, in_dim, out_dim)
-    
-    def test_random_backward_batch(self):
-        for _ in range(n_tests):
-            in_dim = random.randint(1, max_dim)
-            out_dim = random.randint(1, max_dim)
-            batch_size = random.randint(1, max_batch_size)
-            self._backward_batch(batch_size, in_dim, out_dim)
-    
-
-testlin = TestLinear()
-testlin.test_random_backward_batch()
+            self._backward(batch_size, in_dim, out_dim)
