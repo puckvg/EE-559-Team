6dae505f2014f81bc23da772feffbea0c96a52d5
puck <puck.vangerwen@gmail.com>
2021-05-15 17:36:06 +0200

    linear docstrings and TODO check string representation for linear

--- Project2/nn/linear.py
+++ Project2/nn/linear.py
@@ -2,28 +2,33 @@ import torch, math
 from nn.module import Module
 
 class Layer(Module):
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        """Return the string representation of the layer"""
+        pass
+
     def update_param(self, *args, **kwargs):
-        """ Update the params of the Layer based on the cached gradients """
+        """Update the params of the Layer based on the cached gradients"""
         pass
 
     def _grad_local(self, x):
         pass
 
     def param(self):
-        """ Return the params of the Layer. """
+        """Return the params of the Layer."""
         pass
 
 
 class Linear(Layer):
 
     def __init__(self, dim_in, dim_out):
-        """ Initialize object of type Linear with random parameters.
-        Args:
-            dim_in: int. Dimension of input.
-            dim_out int. Dimension of output.
+        """Initialize object of type Linear with random parameters.
 
-        Returns:
-            output: Linear
+        Args:
+            dim_in (int): Dimension of input.
+            dim_out (int): Dimension of output.
         """
         super().__init__()
         self.dim_in = dim_in
@@ -46,29 +51,33 @@ class Linear(Layer):
 
     def __str__(self):
         # Bias is always true so far
+        # TODO what do you mean? bias is not an argument 
         return f"Linear(in_features={self.dim_in}, out_features={self.dim_out}, bias=True)"
 
     def forward(self, x):
-        """ Calculate output.
+        """Calculate output.
+
         Args:
-            x: torch.tensor. Input tensor of size (batch_size, input_dim)
+            x (torch.tensor): Input tensor of size (batch_size, input_dim)
         Returns:
-            output: torch.tensor. Output tensor of size (batch_size, output_dim)
+            output (torch.tensor): Output tensor of size (batch_size, output_dim)
         """
         if len(x.shape) == 1:
             x = x.reshape(1, -1)
         elif len(x.shape) >= 3:
-            raise NotImplementedError("Linear not implement for imput of 3 or more dimensions!")
+            raise NotImplementedError("Linear not implemented for input of 3 or more dimensions!")
         
         w, b = self.param()
-        return w.mm(x.T).T + b
+        output = w.mm(x.T).T + b
+        return output
 
     def backward(self, dy):
-        """ Compute gradients of input and parameters.
+        """Compute gradients of input and parameters.
+
         Args:
-            dy: torch.tensor: Backpropagated gradient from the next layer.
+            dy (torch.tensor): Backpropagated gradient from the next layer.
         Returns:
-            output: torch.tensor: Gradient.
+            output (torch.tensor): Gradient.
         """
 
         # Read local gradients from cache
@@ -88,7 +97,7 @@ class Linear(Layer):
         """
         
         # Fast fix for dy dimension problem
-        """ There is something I haven't figured out with the dimensions yet: I belive that the dimension
+        """There is something I haven't figured out with the dimensions yet: I belive that the dimension
         of the loss function is somehow wrong or incompatible. This fix worked for me, but we should try 
         to understand the problem and fix it properly."""
         if dy.size(1) != dx_loc.size(0): dy = dy.T
@@ -98,22 +107,24 @@ class Linear(Layer):
         self.cache['dx_glob'] = dy.mm(dx_loc).T
         self.cache['dw_glob'] = dw_loc.T.mm(dy).T
         self.cache['db_glob'] = dy.sum(dim=0)
-        
-        return self.cache['dx_glob']
+        output = self.cache['dx_glob']
+        return output
 
     def param(self):
-        """ Get parameters of the linear layer from the cache.
+        """Get parameters of the linear layer from the cache.
+
         Returns:
-            w, b: torch.tensor.
+            w, b (torch.tensor) : weight and bias of linear layer.
         """
         w = self.cache['w']
         b = self.cache['b']
         return w, b
 
     def _grad_local(self, x):
-        """ Compute local gradients of Linear with respect to input and parameters. Store the gradients in the cache for the backward step.
+        """Compute local gradients of Linear with respect to input and parameters. Store the gradients in the cache for the backward step.
+
         Args:
-            x: torch.tensor. Input tensor.
+            x (torch.tensor): Input tensor.
         """
         w, _ = self.param()
         
@@ -122,19 +133,21 @@ class Linear(Layer):
         self.cache['db_loc'] = 1
 
     def _init_param(self):
+        """Initialize parameters from uniform distribution"""
         stdv = 1. / math.sqrt(self.cache['w'].size(1))
         self.cache['w'].uniform_(-stdv, stdv)
         self.cache['b'].uniform_(-stdv, stdv)
     
     def _update_params(self, optim, lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-8):
-        """ Update the parameters of this module according to the opimizer
+        """Update the parameters of this module according to the opimizer
             and the cached gradients 
             
             Args:
-                lr : step size (for all optimizers)
-                beta_1 : first order exp decay (for adam)
-                beta_2 : second order exp decay (for adam)
-                epsilon : small param to prevent zero division (for adam)
+                optim (str) : optimizer option (sgd or adam)
+                lr (float): step size (for all optimizers)
+                beta_1 (float): first order exp decay (for adam)
+                beta_2 (float): second order exp decay (for adam)
+                epsilon (float): small param to prevent zero division (for adam)
         """
         
         if optim == 'sgd':
@@ -176,4 +189,4 @@ class Linear(Layer):
             self.cache['b'] = b
             
         else:
-            raise NotImplementedError
+            raise NotImplementedError, "Only SGD and Adam optimizers have been implemented!"
