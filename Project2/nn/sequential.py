79c2efac7bf42e4e10311045e38b18d859ad072d
puck <puck.vangerwen@gmail.com>
2021-05-15 18:04:55 +0200

    sequential docstrings

--- Project2/nn/sequential.py
+++ Project2/nn/sequential.py
@@ -6,8 +6,10 @@ from nn.linear import Layer
 class Sequential(Module):
     def __init__(self, modules, loss_fn):
         """ Create a new sequential network.
+
         Args:
-            modules: list(Module). List of modules.
+            modules (list(Module)): List of modules.
+            loss_fn (str): loss function.
         """ 
         super().__init__()
         self.modules = modules
@@ -21,7 +23,7 @@ class Sequential(Module):
         return out
 
     def print(self):
-        """ print model architecture"""
+        """Print model architecture."""
         print("Sequential((")
         for module in self.modules: 
             print(f"{module}, ")
@@ -32,29 +34,42 @@ class Sequential(Module):
         return out
 
     def forward(self, x):
+        """Perform forward pass.
+
+        Args:
+            x (torch.tensor): Input tensor 
+
+        Returns: 
+            out (torch.tensor): Output tensor
+        """
         out = x
         for module in self.modules:
             out = module(out)
         return out
 
     def backward(self):
+        """Perform backward pass."""
         dy = self.loss_fn.backward()
         for module in reversed(self.modules):
             dy = module.backward(dy)
     
     def loss(self, x, y):
+        """Compute loss between two tensors.
+
+        Args: 
+            x (torch.tensor) : Input tensor
+            y (torch.tensor) : Target tensor 
+        """
         loss = self.loss_fn(x, y)
         return loss
 
     def update_params(self, optim, lr):
-        """ Update the parameters of the network iteratively
+        """Update the parameters of the network iteratively
             according to the cached gradients at each module. 
 
         Args:
-            optim: string. The optimizer to use.
-                Example: 'adam', 'sgd'
-            lr: float. Learning rate
-
+            optim (string): The optimizer to use. options are 'adam' or 'sgd'
+            lr (float): Learning rate
         """
         for module in self.modules:
             if isinstance(module, Layer):
@@ -62,11 +77,29 @@ class Sequential(Module):
 
     ### Methods for trainer ###
     def training_step(self, x, y):
+        """Training step.
+
+        Args: 
+            x (torch.tensor): Input tensor 
+            y (torch.tensor): Target tensor 
+
+        Returns: 
+            loss (torch.tensor): computed loss
+        """
         out = self.forward(x)
         loss = self.loss(out, y)
         return loss
 
     def validation_step(self, x, y):
+        """Validation step.
+
+        Args: 
+            x (torch.tensor): Input tensor
+            y (torch.tensor): Target tensor 
+
+        Returns: 
+            loss (torch.tensor): computed loss
+        """ 
         out = self.forward(x)
         loss = self.loss(out, y)
         return loss
